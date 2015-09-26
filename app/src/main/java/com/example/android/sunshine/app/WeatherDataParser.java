39440fa23d88077b713d9b420f61b1a925cbd355
Rae Wree <RaeW@users.noreply.github.com>
2015-09-26 09:40:54 -0600

    Switching dev machines

---
+++ app/src/main/java/com/example/android/sunshine/app/WeatherDataParser.java
@@ -0,0 +1,13 @@
+package com.example.android.sunshine.app;
+
+import org.json.JSONException;
+
+/**
+ * Created by Rae on 9/25/2015.
+ */
+public class WeatherDataParser {
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int day)
+        throws JSONException {
+            return -1;
+    }
+}
